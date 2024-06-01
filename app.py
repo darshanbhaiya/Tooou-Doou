c0cae4c670f042c1e0eff8938a0b54a79fcc183c
Darshan Bhaiya <darshanbhaiya0403@gmail.com>
2024-06-01 18:40:44 +0530

    all files added

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import pytz
 
 app = Flask(__name__)  
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db=SQLAlchemy(app)
 
+user_tz = pytz.timezone('Asia/Kolkata')
 class ToDo(db.Model):
     sno=db.Column(db.Integer,primary_key=True)
     title=db.Column(db.String(200),nullable=False)
     desc=db.Column(db.String(500),nullable=False)
-    date_created=db.Column(db.DateTime,default=datetime.utcnow)
+    date_created=db.Column(db.DateTime, default=lambda: datetime.now(user_tz))
 
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
